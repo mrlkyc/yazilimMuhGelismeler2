d59242cb0942d45c10411454124dc7401166c02e
Maral Kıyıcı <maralkyc@gmail.com>
2024-11-06 21:17:52 +0300

    toplama endpointi tanımlandı

--- src/main/java/com/calculator/Calculator/calculatorController.java
+++ src/main/java/com/calculator/Calculator/calculatorController.java
@@ -1,4 +1,19 @@
 package com.calculator.Calculator;
 
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/hesapla")
 public class calculatorController {
+
+
+    @GetMapping("/topla")
+
+    public int topla(@RequestParam int deger1,@RequestParam int deger2){
+
+        return deger1+ deger2;
+
+    }
+
 }
